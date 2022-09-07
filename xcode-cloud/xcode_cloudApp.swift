56960fe46ff1d242dba7a98e7fc6212e83acf934
amr1440 <amr.iosdeveloper@gmail.com>
2022-09-07 13:02:51 +0300

    Initial Commit

---
+++ xcode-cloud/xcode_cloudApp.swift
@@ -0,0 +1,17 @@
+//
+//  xcode_cloudApp.swift
+//  xcode-cloud
+//
+//  Created by Amr Sayed on 07/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct xcode_cloudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
